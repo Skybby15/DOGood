83c1bc561079f7dbf7a35f37d991b8d38e908bb1
Skybby15 <savaclaudiu42@gmail.com>
2025-07-13 16:31:23 +0300

    Main form created , size and position hard-coded

--- FrontEnd/(start)/Content/Search/SearchTab.tsx
+++ FrontEnd/(start)/Content/Search/SearchTab.tsx
@@ -1,20 +1,63 @@
-import { View,Text,StyleSheet } from "react-native";
-
+import { View,Text,StyleSheet, TextInput, TouchableOpacity } from "react-native";
+import { Ionicons } from '@expo/vector-icons'
 
 export default function SearchTab()
 {
     return(
-        <View style={{flex:1,backgroundColor:'red'}}>
-            <View style={{flex:1,backgroundColor:'blue'}}></View>
+        <View style={styles.container}>
+            <View style={styles.searchBar}>
+                <TouchableOpacity style={styles.searchButton}>
+                    <Ionicons name="search" size={20} color="black" />
+                </TouchableOpacity>
+                <TextInput 
+                inputMode="search"
+                maxLength={300}
+                style={styles.searchTextInput}>
+                    Search
+                </TextInput>
+                <TouchableOpacity style={styles.searchClearbutton}>
+                    <Ionicons name="close" size={20} color="black" />
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
         backgroundColor: '#A8B5DB',
-        flex:1,
+    },
+
+    searchBar: {
+        flexDirection: 'row',
+
+        position: 'absolute',
+        height: 40,
+        top: 50,
         alignItems: 'center',
-        justifyContent: 'center'
-    }
+        justifyContent: 'center',
+        alignSelf: 'center',
+
+        width: '90%',
+
+        borderWidth: 1.3,
+        borderRadius: 13,
+
+        backgroundColor: 'transparent',
+
+    },
+
+    searchTextInput: {
+        width: '80%',
+    },
+
+    searchButton: {
+
+        paddingHorizontal: 10,
+    },
+
+    searchClearbutton: {
+        paddingHorizontal: 10,
+    },
 })
